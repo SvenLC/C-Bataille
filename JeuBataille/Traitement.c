6ac01956a9b3cb1128659b6d9e402ea0f0ce6cab
Sven Le Cann <sven.lecann@live.fr>
2018-03-15 11:49:27 +0100

    Définition de l'aglo

--- JeuBataille/Traitement.c
+++ JeuBataille/Traitement.c
@@ -18,6 +18,11 @@ void InitialisationPartie()
 
 }
 
+void FinPartie()
+{
+	//On test si un joueur à son tas vide une fois que son paquet de carte est vide
+}
+
 void TirageDesCartes(int JeuCartes[]) 
 {
 	//On tire les 54 cartes aléatoirement
@@ -28,5 +33,28 @@ void DistributionDesCartes(int JeuCartesJoueur1[], int JeuCartesJoueurs2[])
 {
 	//On réparati les 54 cartes dans le jeu des joueurs 1 et 2
 }
+
+void DeroulementTour()
+{
+	//On prend la dernière carte du jeu des joueurs et on les compare
+	
+	//TODO Comparer la dernière carte de la liste
+
+	//Si les cartes n'ont pas la même valeur, le joueur qui a la plus grande valeur remporte le pli
+	//On met les cartes du pli dans le tas du joueur qui à remporté le pli
+
+	//Si les cartes ont la même valeur
+	//On prend la dernière carte du jeu des joueurs et on la met face caché dans le pli
+	//On prend la dernière carte du jeu des joueur et on la compare
+	//On boucle jusqu'a ce qu'un joueur remporte le pli
+
+}
+
+void RecuperationDuTas()
+{
+	//Lorsque le jeu d'un joueur est vide on prend le tas et on le met dans le jeu du joueur
+}
+
+
 
 
